6dab7ec8b38dbe54e3d33f5ded52142fc84abc86
ATL-N <atlcoccus@gmail.com>
2025-12-28 09:09:11 +0000

    udated to prisma v7

--- app/lib/prisma.js
+++ app/lib/prisma.js
@@ -3,6 +3,18 @@ import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis;
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+const getPrismaClient = () => {
+  return new PrismaClient({
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL,
+      },
+    },
+  });
+};
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+export const prisma = globalForPrisma.prisma || getPrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+    globalForPrisma.prisma = prisma;
+}
